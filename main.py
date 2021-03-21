fc1989f610a1efc2be575fc4fa15eb3d1633ef59
Anis Aggoun <anisaggoun06@gmail.com>
2021-03-21 13:08:28 +0100

    Added some comments

--- main.py
+++ main.py
@@ -9,24 +9,27 @@ import pygame
 from pygame.locals import *
 import numpy as np
 import time
+
 #############
 programIcon = pygame.image.load('ikon.png')
-
-#############
-
 WIDTH, HEIGHT = 450, 450
 WHITE = (255,255,255)
 GREEN = (50,205,50)
 GREY = (169,169,169)
 BLACK = (0,0,0)
 FPS = 60
-
-###################
 pygame.font.init()
 font = pygame.font.SysFont(None, 25)
 ###################
 
 def main():
+    """
+    This function displays an initialized grid using the Grid class then displays the solver algorithm steps as soon as the user left clicks
+    anywhere in the window.
+    ----
+    NOTES : 
+    Although the Grid class works with any grid dimension. This displaying part has been written for the most common 9x9 case. 
+    """
     clock = pygame.time.Clock()
     run = True
     draw_grid()
@@ -43,20 +46,6 @@ def main():
 
         clock.tick(FPS)
     pygame.display.quit()
-
-def writer(anumber, crds_case, color = BLACK ):
-    if anumber == 0:
-        pygame.draw.rect(WIN, WHITE, [crds_case[0] + 5,crds_case[1] + 5,30,30])
-        #pygame.draw.rect(WIN, BLACK, [0,300-3,450,6])
-    else :
-        number_text = font.render(str(anumber), True, color)
-        WIN.blit(number_text, [crds_case[0]+20,crds_case[1]+20])
-    pygame.display.update()
-
-def algo_displayer():
-    for ele in une_grille.registre:
-        time.sleep(0.05)
-        writer(ele[0], tab0[ele[1]][ele[2]], color = GREEN)
 
 def draw_grid():
     WIN.fill(WHITE)
@@ -75,6 +64,19 @@ def draw_grid():
         for j in range(9):
             if une_grille.grid0[i][j] != 0 :
                 writer(une_grille.grid0[i][j], tab0[i][j])
+
+def algo_displayer():
+    for ele in une_grille.registre:
+        time.sleep(0.05) #This function is actually slowed down so the user can follow the algorithm steps.
+        writer(ele[0], tab0[ele[1]][ele[2]], color = GREEN)
+
+def writer(anumber, crds_case, color = BLACK ):
+    if anumber == 0:
+        pygame.draw.rect(WIN, WHITE, [crds_case[0] + 5,crds_case[1] + 5,30,30])
+    else :
+        number_text = font.render(str(anumber), True, color)
+        WIN.blit(number_text, [crds_case[0]+20,crds_case[1]+20])
+    pygame.display.update()
 
 tab0 = np.zeros([9,9], dtype=list)
 for i in range(tab0.shape[0]):
